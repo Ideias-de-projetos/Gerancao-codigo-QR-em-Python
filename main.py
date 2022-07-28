2e2cc40c3658d9eb6c79e80cb6445423066237e3
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-28 10:18:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,16 +20,35 @@ import cv2
 # 1
 # criar um objeto pyqrcode
 url1 = pyqrcode.create("https://meet.google.com/mhu-wczm-bbg")
-nomeArquivo = "qRSalaAula"
-url1.png(nomeArquivo + ".png")
+nomeArquivo1 = "qRSalaAula"
+url1.png(nomeArquivo1 + ".png")
 url1.show()
 
 
 # 2
 # criar um objeto pyqrcode
 url2 = pyqrcode.create("https://www.youtube.com/")
-nomeArquivo = "qRyoutube"
-url2.png(nomeArquivo + ".png", scale=10)
+nomeArquivo2 = "qRyoutube"
+url2.png(nomeArquivo2 + ".png", scale=10)
 url2.show()
+
+
+# ler qrCode
+# 3
+# criar um objeto cv2
+imagem1 = cv2.imread(nomeArquivo1 + ".png")
+imagem2 = cv2.imread(nomeArquivo2 + ".png")
+# 4
+# criar um objeto cv2
+cv2.imshow("qrCode1", imagem1)
+cv2.imshow("qrCode2", imagem2)
+# 5
+# criar um objeto cv2
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# 6
+# criar um objeto cv2
+cv2.imwrite("qrCode1.png", imagem1)
+cv2.imwrite("qrCode2.png", imagem2)
 
 
