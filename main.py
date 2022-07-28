951a1296647e23c4cd0292bde5e1aaecedcee0a1
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-28 10:09:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,23 @@
 # https://www.youtube.com/
 
 #### importar as bibliotecas  ####
+# pip install pyqrcode
+# pip3 install pypng
 import pyqrcode
 import png
 
 # instalação do OpenCV
 # pip install opencv-python
 import cv2
-##### fim da importação #####
+##### fim da importação ####  #
 
 
 # 1
 # criar um objeto qrcode
-url1 = qrcode.make("https://meet.google.com/mhu-wczm-bbg")
-url1.save('qRsalaAula.png')
+url1 = pyqrcode.create("https://meet.google.com/mhu-wczm-bbg")
+nomeArquivo = "qRSalaAula"
+url1.png(nomeArquivo + ".png")
+url1.show()
 
 
 
