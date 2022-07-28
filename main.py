7eb566139021e9033ecd1658ed024a65478f4687
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-28 09:44:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,13 @@
 
 # importar as bibliotecas
 import qrcode
+# instalação do OpenCV
+# pip install opencv-python
 import cv2
 
 # 1
 # criar um objeto qrcode
-image1 = qrcode.make('https://meet.google.com/mhu-wczm-bbg')
+image1 = qrcode.make("https://meet.google.com/mhu-wczm-bbg")
 image1.save('qRsalaAula.png')
 
 
