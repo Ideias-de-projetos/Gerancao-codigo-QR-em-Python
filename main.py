4241d57efc608aad9f00ba1ac359fbb33728cbfe
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-28 09:39:38 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#
+#
+#
+# links
+# https://meet.google.com/mhu-wczm-bbg
+# https://www.youtube.com/
+
+# importar as bibliotecas
+import qrcode
+import cv2
+
+# 1
+# criar um objeto qrcode
+image1 = qrcode.make('https://meet.google.com/mhu-wczm-bbg')
+
+
+
